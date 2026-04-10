6177701ed7a6d56661c8bfc72074c803ea402055
Jxvz01 <jeevanh259@gmail.com>
2026-04-10 23:14:21 +0530

    refactor: Add barrel export for components

---
+++ src/components/index.js
@@ -0,0 +1,7 @@
+export * from './Toast';
+export * from './SkeletonCard';
+export * from './Counter';
+export * from './AchievementHub';
+export * from './SystemBoot';
+export * from './SidePanel';
+export * from './AuthForm';
